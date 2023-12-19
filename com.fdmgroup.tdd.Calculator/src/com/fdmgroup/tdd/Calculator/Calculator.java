ed9860723ae2bebaffd28cfef5caf525c2279446
Matthew Chanco <Matthew.Chanco@fdmgroup.com>
2023-12-19 16:10:16 +1100

    Initial submission of calculator project by Matthew Chanco. Calculator should be working as expected, except for fractional exponents at this point.

---
+++ com.fdmgroup.tdd.Calculator/src/com/fdmgroup/tdd/Calculator/Calculator.java
@@ -0,0 +1,502 @@
+package com.fdmgroup.tdd.Calculator;
+
+/**
+ * This class acts as a calculator that evaluates mathematical expressions provided as strings.
+ * It supports basic arithmetic operations, exponentiation, and the handling of parentheses.
+ * The calculator follows the standard order of operations (BODMAS). All operations are made 
+ * to be evaluated in a recursive manner. No loops were used in the implementation of any function.
+ * All whitespace is removed throughout all operations.
+ *
+ * 
+ * @author Matthew Chanco
+ * @version 19/12/2024
+ */
+
+public class Calculator implements ICalculator {
+
+	// Basic math operators: Addition, Subtraction, Multiplication, Division
+
+	/**
+	 * Basic addition operation
+	 * @param x First number
+	 * @param y Second number
+	 * @return The sum of x and y.
+	 */
+
+	public double add(double x, double y) {
+		return x + y;	
+	}
+
+
+	/**
+	 * Basic subtraction operation
+	 * @param x First number
+	 * @param y Second number
+	 * @return The difference between x and y.
+	 */
+
+	public double minus(double x, double y) {
+		return x - y;
+	}
+
+
+	/**
+	 * Basic multiplication operation
+	 * @param x First number
+	 * @param y Second number
+	 * @return The product of x and y.
+	 */
+
+	public double multiply(double x, double y) {
+		return x * y;
+	}
+
+
+	/**
+	 * Basic division operation
+	 * @param x Dividend
+	 * @param y Divisor
+	 * @return The quotient of x when divided by y.
+	 */
+
+	public double divide(double x, double y) {
+
+		if ( y == 0 ) {
+
+			throw new IllegalArgumentException("Dividing by zero. Not a valid operation.");
+		}
+
+		return x / y;
+	}
+
+
+	// Exponential operator: with ^ as the operator, integer exponents for now
+
+	/**
+	 * Evaluates the power of a base number raised to an exponent. It evaluates the 
+	 * expression recursively.
+	 * @param base Base number
+	 * @param exponent Exponent
+	 * @return The result of base raised to the power of the exponent.
+	 */
+
+	public double power(double base, double exponent) {
+
+		if ( exponent == 0 ) {
+
+			return 1;
+		}
+
+		if ( exponent < 0 ) {
+
+			return 1 / power(base, -exponent);
+		}
+
+		return base * power(base, exponent - 1);
+	}
+
+	// Cleaner Methods
+
+	// Method to adjust expressions with combinations of operators ex. "++", "--", "+-", "-+", ...
+
+	/**
+	 * Simplifies the expression by combining redundant operations such as:
+	 * "++", "+-", "--", "-+", "*+", and "/+"
+	 * @param expression Mathematical expression to be simplified.
+	 * @return Simplified expression
+	 */
+
+	private String simplifyExpression ( String expression ) {
+
+		if ( expression.contains("++") ) {
+
+			int index = expression.indexOf("++");
+			return simplifyExpression(expression.substring(0, index) + "+" + (index + 2 < expression.length() ? expression.substring(index + 2) : ""));
+
+		} else if ( expression.contains("--") ) {
+
+			int index = expression.indexOf("--");
+			return simplifyExpression(expression.substring(0, index) + "+" + (index + 2 < expression.length() ? expression.substring(index + 2) : "") );
+
+		} else if ( expression.contains("+-") ) {
+
+			int index = expression.indexOf("+-");
+			return simplifyExpression(expression.substring(0, index) + "-" + (index + 2 < expression.length() ? expression.substring(index + 2) : "") );
+
+		} else if ( expression.contains("-+") ) {
+
+			int index = expression.indexOf("-+");
+			return simplifyExpression(expression.substring(0, index) + "-" + (index + 2 < expression.length() ? expression.substring(index + 2) : "") );
+
+		} else if ( expression.contains("*+") ) {
+
+			int index = expression.indexOf("*+");
+			return simplifyExpression(expression.substring(0, index) + "*" + (index + 2 < expression.length() ? expression.substring(index + 2) : "") );
+
+		} else if ( expression.contains("/+") ) {
+
+			int index = expression.indexOf("/+");
+			return simplifyExpression(expression.substring(0, index) + "/" + (index + 2 < expression.length() ? expression.substring(index + 2) : "") );
+
+		}
+
+		return expression;
+	}
+
+	// Method to adjust expression if leading with a '+' or '-'
+
+	/**
+	 * Cleans the expression by removing any leading '+' or '-' signs to make it
+	 * readable by the code.
+	 * @param expression Mathematical expression to be cleaned
+	 * @return Cleaned operation
+	 */
+
+	private String cleanExpression( String expression ) {
+
+		if ( expression.charAt(0) == '+' ) {
+
+			return expression.substring(1);
+
+		} else if ( expression.charAt(0) == '-' ) {
+
+			return "0" + expression;
+		}
+
+		return expression;
+	}
+
+	// Method to check for invalid input.
+
+	/**
+	 * Checks for any invalid operations within the expression such as:
+	 * "+*", "-*", "**", "/*", "+/", "-/", "//"..
+	 * It does not return anything.
+	 * 
+	 * @param expression Mathematical expression to be checked.
+	 * @throws IllegalArgumentException If invalid operators are detected.
+	 */
+
+	private void checkInvalidOperations( String expression ) {
+		if ( expression.contains("-*") || expression.contains("+*") || expression.contains("-/")
+				|| expression.contains("+/") || expression.contains("**") || expression.contains("//")) {
+
+			throw new IllegalArgumentException("Invalid argument detected.");
+		}
+	}
+
+	
+	// BODMAS Methods
+
+	// Check for Parenthesis
+
+	/**
+	 * This is the first function to be called when running the code.
+	 * 
+	 * Evaluates the mathematical expression inside parentheses (B) and recursively 
+	 * replaces the expression with the evaluated value to remove the brackets "()"
+	 * until all are evaluated.
+	 * 
+	 * It finds the innermost parenthesis first then works its way outwards until
+	 * all the parentheses are removed. It then calls the function for evaluating
+	 * exponents.
+	 * 
+	 * @param expression Mathematical expression to be evaluated that contains "()"
+	 * @return Calls the function again to detect and evaluate expressions in parenthesis.
+	 * If none, calls the function for evaluating exponents.
+	 */
+
+	private double performParenthesesOperations(String expression) {
+		expression = expression.replaceAll(" ", "");
+
+
+		if (!expression.contains("(")) {
+			return performExponentsOperations(expression);
+		}
+
+		// Find the innermost parentheses then evaluates
+		int start = expression.lastIndexOf('(');
+		int end = findClosingParenthesis(expression, start);
+		String innerExpression = expression.substring(start + 1, end);
+		double innerResult = performExponentsOperations(innerExpression);
+
+
+		// Updates the expression by removing brackets and calls the function again with the new expression
+		String newExpression = expression.substring(0, start) + innerResult + expression.substring(end + 1);
+
+		return performParenthesesOperations(newExpression);
+	}
+
+	
+	
+	
+	// This method detects the first closing bracket with respect to the first opening bracket.
+	private int findClosingParenthesis ( String expression, int index ) {
+
+		if ( expression.charAt(index) == ')' ) {
+
+			return index;
+		}
+
+		return findClosingParenthesis ( expression, index + 1);
+
+	}
+
+
+	// Check for Exponents
+
+	/**
+	 * This is the second function to be called after clearing all the expressions in parenthesis.
+	 * 
+	 * Evaluates any exponential(O) operations symbolized by a caret "^" recursively until all
+	 * carets are evaluated. Note that it only evaluates the first number succeeding the symbol
+	 * as the exponent and the first number preceding the symbol as the base. Any other inputs
+	 * must be enclosed in a bracket to be properly evaluated.
+	 * 
+	 * @param expression Mathematical expression to be evaluated that contains ^
+	 * @return Calls the function again to detect and evaluate exponential expressions.
+	 * If none, calls the function for evaluating multiplication/division operations.
+	 */
+
+	private double performExponentsOperations ( String expression ) {
+		expression = expression.replaceAll(" ", "");
+
+		if (!expression.contains("^")) {
+			return performMDOperations( expression );
+		}
+
+		// Find the rightmost exponential operation then evaluates
+		int start = expression.lastIndexOf('^');
+		
+		// Splits the exponential expression into base and exponent
+		int baseStart = findBaseStart( expression, start - 1);
+		int exponentEnd = findExponentEnd( expression, start + 1);
+		
+		double base = Double.parseDouble(expression.substring(baseStart, start));
+		double exponent = Double.parseDouble(expression.substring(start + 1, exponentEnd + 1));
+		
+		double result = power(base, exponent);
+
+
+		// Updates the expression to remove the ^ symbol then calls the function with the new expression
+		String newExpression = expression.substring(0, baseStart) + result + expression.substring(exponentEnd + 1);
+
+		return performExponentsOperations(newExpression);
+	}
+
+	
+	
+	
+	// This method finds the first number to the left of the ^ symbol
+	private int findBaseStart(String expression, int index) {
+		
+		// Base case: Start of the string or previous character is an operator
+		if (index < 0 || isOperator(expression.charAt(index))) {
+			return index + 1;
+		}
+		
+
+		return findBaseStart(expression, index - 1);
+	}
+
+	// This method finds the first number to the right of the ^ symbol
+	private int findExponentEnd(String expression, int index) {
+		
+		// Base case: End of the string or next character is an operator
+		if (index >= expression.length() - 1 || isOperator(expression.charAt(index + 1))) {
+			return index;
+		}
+
+		return findExponentEnd(expression, index + 1);
+	}
+
+
+
+	// Check for Multiplication/Division Operations
+	
+	/**
+	 * This is the next function to be called after evaluating exponential expressions.
+	 * 
+	 * Evaluates any multiplication(M) or division(D) operations in the expression, 
+	 * symbolized by an asterisk "*" or a backslash "/". Recursively calls the function
+	 * until all MD operations are done.
+	 * 
+	 * The code works such that it splits the expression into two at the index where
+	 * the operator is. It then extracts the numbers directly next to the operator
+	 * on both sides, including the '-' sign, indicating if it is a subtraction operator
+	 * or if it signifies a negative sign.
+	 *  
+	 * @param expression Mathematical expression to evaluate * and / operators
+	 * @return Calls the function again to detect and evaluate MD expressions.
+	 * If none, calls the function for evaluating addition/subtraction operations.
+	 */
+
+	private double performMDOperations(String expression) {
+		
+		// Cleaner methods to ensure string readability for the code
+		expression = cleanExpression(expression);
+		expression = simplifyExpression(expression);
+
+
+		if (!expression.contains("*") && !expression.contains("/")) {
+			return performASOperations(expression);
+		}
+
+		if (expression.contains("*")) {
+			int index = expression.lastIndexOf("*");
+
+			// This is where the expression is split into two parts
+			String left = expression.substring(extractLeftNumber(expression, index - 1), index);
+			String right = expression.substring(index + 1, extractRightNumber(expression, index+1));
+
+			double result = multiply(performMDOperations(left), performMDOperations(right));
+
+			// Updates the expression then calls the function again with the new expression
+			String newExpression = expression.substring(0, extractLeftNumber(expression, index))
+					+ result
+					+ expression.substring(extractRightNumber(expression, index + 1));
+
+
+			return performMDOperations(newExpression);
+		}
+
+		if (expression.contains("/")) {
+			int index = expression.lastIndexOf("/");
+
+			// This is where the expression is split into two parts
+			String left = expression.substring(extractLeftNumber(expression, index - 1), index);
+			String right = expression.substring(index + 1, extractRightNumber(expression, index+1));
+
+			double result = divide(performMDOperations(left), performMDOperations(right));
+
+			// Updates the expression then calls the function again with the new expression
+			String newExpression = expression.substring(0, extractLeftNumber(expression, index))
+					+ result
+					+ expression.substring(extractRightNumber(expression, index + 1));
+
+
+			return performMDOperations(newExpression);
+		}
+
+		return performMDOperations(expression);
+
+	}
+
+	
+	
+
+	// Methods to extract the numbers on both sides of the operator
+	private int extractLeftNumber(String expression, int index) {
+
+		// Base case: Start of the string or previous character is an operator
+		if (index <= 0 || isOperator(expression.charAt(index - 1))) {
+			return index;
+		}
+
+		return extractLeftNumber(expression, index - 1);
+	}
+
+
+	private int extractRightNumber(String expression, int index) {
+
+		// Base case: End of the string
+		if (index >= expression.length()) {
+			return index;
+		}
+
+		if (isOperator(expression.charAt(index))) {
+
+			if ( expression.charAt(index) == '-' && !isOperator(expression.charAt(index-1)) ) {
+
+				return index;
+			}
+
+			// Directly handles negative numbers
+			if (expression.charAt(index) == '-' && index + 1 < expression.length() 
+					&& !isOperator(expression.charAt(index + 1))) {
+
+
+				return extractRightNumber(expression, index + 1);
+			}
+			return index;
+		}
+
+		return extractRightNumber(expression, index + 1);
+	}
+
+	
+	
+	
+	// Method to detect if the character in check is an operator
+	private boolean isOperator(char c) {
+		return c == '+' || c == '-' || c == '*' || c == '/' || c == '^';
+	}
+
+
+	// Check for Addition/Subtraction Operations
+
+	/**
+	 * This is the last function to be called after performing multiplication and division.
+	 * 
+	 * Evaluates any addition(A) or subtraction(S) operations in the expression, 
+	 * symbolized by a plus "+" or a minus "-". Recursively calls the function
+	 * until all AS operations are done. It is expected that all other operators
+	 * do not exist at this point except for "+" and "-".
+	 *  
+	 * @param expression Mathematical expression to evaluate + and - operators
+	 * @return Calls the function again to detect and evaluate AS expressions.
+	 * If none, parses the final expression into a double.
+	 */
+	
+	
+	private double performASOperations ( String expression ) {
+
+		if ( expression.charAt(0) == '+' || expression.charAt(0) == '-' ) {
+
+			expression = cleanExpression(expression);
+			expression = simplifyExpression(expression);
+
+		}
+
+		if ( expression.contains("+") ) {
+
+			int index = expression.lastIndexOf("+");
+
+			String left = expression.substring(0, index);
+			String right = expression.substring(index + 1);
+
+
+			return add(performASOperations(left), performASOperations(right));
+
+		} else if ( expression.contains("-") ) {
+
+			int index = expression.lastIndexOf("-");
+
+			String left = expression.substring(0, index);
+			String right = expression.substring(index + 1);
+
+
+			return minus(performASOperations(left), performASOperations(right));
+		}
+
+		return Double.parseDouble(expression);
+	}
+
+
+	// Interface method for the user to use the calculator.
+	@Override
+	public double evaluate(String expression) {
+		
+		// Cleans the expression of whitespace
+		expression = expression.replaceAll(" ", "");
+		
+		// Preliminary check for invalid operations
+		checkInvalidOperations(expression);
+		
+		// Cleans redundant operators
+		expression = simplifyExpression(expression);
+
+		return performParenthesesOperations(expression);
+	}
+
+}
---
+++ com.fdmgroup.tdd.Calculator/test/com/fdmgroup/tdd/Calculator/CalculatorTest.java
@@ -0,0 +1,514 @@
+package com.fdmgroup.tdd.Calculator;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+	
+	Calculator calculator = new Calculator();
+	
+	// Tests for basic math operations
+	
+	@Test
+	void basic_addition_2_plus_3_equals_5() {
+		
+		assertEquals(5, calculator.add((double) 2, (double) 3));
+		
+	}
+	
+	@Test
+	void basic_subtraction_3_minus_2_equals_1() {
+		
+		assertEquals(1,calculator.minus((double) 3, (double) 2));
+		
+	}
+	
+	@Test
+	void basic_multiplication_3_multiply_2_equals_6() {
+		
+		assertEquals(6, calculator.multiply((double) 3, (double) 2));
+	}
+	
+	@Test
+	void basic_division_6_divide_2_equals_3() {
+		
+		assertEquals(3,calculator.divide((double) 6, (double) 2));
+	}
+	
+	// Tests for invalid arguments
+	
+	@Test
+	void invalid_operator_leading_asterisk_expression() {
+		
+		assertThrows(IllegalArgumentException.class, () -> {calculator.evaluate("1+*1");});
+	}
+	
+	@Test
+	void invalid_operator2_leading_asterisk_expression() {
+		
+		assertThrows(IllegalArgumentException.class, () -> {calculator.evaluate("1-*1");});
+	}
+	
+	@Test
+	void invalid_operator_leading_slash_expression() {
+		
+		assertThrows(IllegalArgumentException.class, () -> {calculator.evaluate("1+/1");});
+	}
+	
+	@Test
+	void invalid_operator2_leading_slash_expression() {
+		
+		assertThrows(IllegalArgumentException.class, () -> {calculator.evaluate("1-/1");});
+	}
+	
+	@Test
+	void invalid_operator_leading_asterisk_at_the_end() {
+		
+		assertThrows(IllegalArgumentException.class, () -> {calculator.evaluate("1+1+*1");});
+	}
+	
+	// Tests for performing basic operations from string input
+	
+	@Test
+	void addition_1_plus_2_equals_3_from_string() {
+		double result = 1+2;
+		
+		assertEquals(result, calculator.evaluate("1+2"));
+	}
+	
+	@Test
+	void subtraction_2_minus_1_equals_1_from_string() {
+		double result = 2-1;
+		
+		assertEquals(result, calculator.evaluate("2-1"));
+	}
+	
+	@Test
+	void multiplication_times_1_equals_2_from_string() {
+		double result = 2*1;
+		
+		assertEquals(result, calculator.evaluate("2*1"));
+	}
+	
+	@Test
+	void division_divide_1_equals_2_from_string() {
+		double result = 2/1;
+		
+		assertEquals(result, calculator.evaluate("2/1"));
+	}
+	
+	// Tests for performing double operations "+-", "--", "*-", etc.
+	
+	@Test
+	void double_operator_plus_minus_operation() {
+		double result = 1+-1;
+		
+		assertEquals(result, calculator.evaluate("1+-1"));
+	}
+	
+	@Test
+	void double_operator_minus_plus_operation() {
+		double result = 1-+1;
+		
+		assertEquals(result, calculator.evaluate("1-+1"));
+	}
+	
+	@Test
+	void double_operator_minus_minus_operation() {
+		double result = 1-(-1);
+		
+		assertEquals(result, calculator.evaluate("1--1"));
+	}
+	
+	@Test
+	void double_operator_timus_plus_operation() {
+		double result = 3*+5;
+		
+		assertEquals(result, calculator.evaluate("3*+5"));
+	}
+	
+	@Test
+	void double_operator_divide_plus_operation() {
+		double result = 3.0/+5.0;
+		
+		assertEquals(result, calculator.evaluate("3/+5"));
+	}
+	
+	@Test
+	void double_operator_times_minus_operation() {
+		double result = 1*-4;
+		
+		assertEquals(result, calculator.evaluate("1*-4"));
+	}
+	
+	@Test
+	void double_operator_divide_minus_operation() {
+		double result = 4/-1;
+		
+		assertEquals(result, calculator.evaluate("4/-1"));
+	}
+	
+	
+	// Tests for multiple operations
+	
+	@Test
+	void multiple_operations_1_plus_2_plus_3_equals_6() {
+		double result = 1+2+3;
+		
+		assertEquals(result, calculator.evaluate("1+2+3"));
+	}
+	
+	@Test
+	void multiple_operations_1_minus_2_plus_3_equals_2() {
+		double result = 1-2+3;
+		
+		assertEquals(result, calculator.evaluate("1-2+3"));
+	}
+	
+	@Test
+	void multiple_operations_1_plus_2_plus_3_minus_1_equals_5() {
+		double result = 1+2+3-1;
+		
+		assertEquals(result, calculator.evaluate("1+2+3-1"));
+	}
+	
+	@Test
+	void multiple_operations_1_minus_2_minus_3_equals_negative_4() {
+		double result = 1-2-3;
+		
+		assertEquals(result, calculator.evaluate("1-2-3"));
+	}
+	
+	@Test
+	void multiple_double_operations_1_plus_2_plus_3_equals_6() {
+		
+		assertEquals(6, calculator.evaluate("1++2++3"));
+	}
+	
+	@Test
+	void multiple_double_operator_times_minus_double_operator() {
+		double result = 1*-4*-4;
+		
+		assertEquals(result, calculator.evaluate("1*-4*-4"));
+	}
+	
+	@Test
+	void multiple_operations_with_times_plus_double_operator() {
+		double result = 1*+4*+4;
+		
+		assertEquals(result, calculator.evaluate("1*+4*+4"));
+	}
+	
+	@Test
+	void multiple_double_operators_plus_minus() {
+		double result = 1+-2+-3;
+				
+		assertEquals(result, calculator.evaluate("1+-2+-3"));
+	}
+	
+	@Test
+	void multiple_double_operators_minus_plus() {
+		double result = 1-+2-+3;
+				
+		assertEquals(result, calculator.evaluate("1-+2-+3"));
+	}
+	
+	@Test
+	void multiple_operations_times_then_plus() {
+		double result = 11*2+3;
+		
+		assertEquals(result, calculator.evaluate("11*2+3"));
+	}
+	
+	@Test
+	void multiple_operations_plus_then_times() {
+		double result = 1+22*3;
+		
+		assertEquals(result, calculator.evaluate("1+22*3"));
+	}
+	
+	@Test
+	void multiple_operations_times_and_plus() {
+		double result = 1+22*3+44*5;
+		
+		assertEquals(result, calculator.evaluate("1+22*3+44*5"));
+	}
+	
+	@Test
+	void multiple_operations_times_and_minus() {
+		double result = 1-2*2-3;
+		
+		assertEquals(result, calculator.evaluate("1-2*2-3"));
+	}
+	
+	@Test
+	void multiple_operations2_times_and_minus() {
+		double result = 1-2*22-33;
+		
+		assertEquals(result, calculator.evaluate("1-2*22-33"));
+	}
+	
+	
+	@Test
+	void multiple_operations_with_decimals() {
+		double result = 10.4+1.3*2.6+3.1*4.1/5.1;
+		
+		assertEquals(result, calculator.evaluate("10.4+1.3*2.6+3.1*4.1/5.1"));
+	}
+	
+	
+	@Test
+	void mutiple_operations_with_leading_minus_sign() {
+		double result = -10+1*2+3*4/2;
+		
+		assertEquals(result, calculator.evaluate("-10+1*2+3*4/2"));
+	}
+	
+	@Test
+	void mutiple_operations_with_leading_plus_sign() {
+		double result = +10+1*2+3*4/2;
+		
+		assertEquals(result, calculator.evaluate("+10+1*2+3*4/2"));
+	}
+	
+	@Test
+	void multiple_operations_with_times_plus_double_operator_divided_by_negative() {
+		double result = 1*+44-5/-1;
+		
+		assertEquals(result, calculator.evaluate("1*+44-5/-1"));
+	}
+	
+	
+	// Tests for brackets handling
+	
+	@Test
+	void single_bracket_addition() {
+		double result = (1+2);
+		
+		assertEquals(result, calculator.evaluate("(1+2)"));
+	}
+	
+	@Test
+	void single_bracket_minus() {
+		double result = (4-5);
+		
+		assertEquals(result, calculator.evaluate("(4-5)"));
+	}
+	
+	@Test
+	void single_bracket_times() {
+		double result = (3*7);
+		
+		assertEquals(result, calculator.evaluate("(3*7)"));
+	}
+	
+	@Test
+	void single_bracket_divide() {
+		double result = (6/3);
+		
+		assertEquals(result, calculator.evaluate("(6/3)"));
+	}
+	
+	@Test
+	void single_bracket_multiple_operations() {
+		double result = (1+1-1);
+		
+		assertEquals(result, calculator.evaluate("(1+1-1)"));
+	}
+	
+	@Test
+	void single_bracket_multiple_operations_outside_bracket() {
+		double result = (1-1)+1;
+		
+		assertEquals(result, calculator.evaluate("(1-1)+1"));
+	}
+	
+	@Test
+	void single_bracket_multiple_operations_outside_bracket_times_inside() {
+		double result = (1*1)+1;
+		
+		assertEquals(result, calculator.evaluate("(1*1)+1"));
+	}
+	
+	@Test
+	void single_bracket_multiple_operations_outside_bracket_divide_inside() {
+		double result = (1/1)+1;
+		
+		assertEquals(result, calculator.evaluate("(1/1)+1"));
+	}
+	
+	@Test
+	void single_bracket_multiple_operations_outside_bracket_divide_inside_times_plus_outside() {
+		double result = 5*(6/3)+1;
+		
+		assertEquals(result, calculator.evaluate("5*(6/3)+1"));
+	}
+	
+	@Test
+	void single_bracket_multiple_operations_outside_bracket_with_negative_sign_inside() {
+		double result = 67*87+(-68+89)-38*2;
+		
+		assertEquals(result, calculator.evaluate("67*87+(-68+89)-38*2"));
+	}
+	
+	@Test
+	void single_bracket_multiple_operations_outside_bracket_with_negative_sign_inside_divided_by_bracket() {
+		double result = 67.0*87.0/(-68.0+89.0)-38.0*2.0;
+		
+		assertEquals(result, calculator.evaluate("67*87/(-68+89)-38*2"));
+	}
+	
+	@Test
+	void multiple_bracket_single_operation_plus() {
+		double result = ((1+1));
+		
+		assertEquals(result, calculator.evaluate("((1+1))"));
+	}
+	
+	@Test
+	void multiple_bracket_single_operation_minus() {
+		double result = ((1-1));
+		
+		assertEquals(result, calculator.evaluate("((1-1))"));
+	}
+	
+	@Test
+	void multiple_bracket_single_operation_times() {
+		double result = ((1*1));
+		
+		assertEquals(result, calculator.evaluate("((1*1))"));
+	}
+	
+	@Test
+	void multiple_bracket_single_operation_divide() {
+		double result = ((1/1));
+		
+		assertEquals(result, calculator.evaluate("((1/1))"));
+	}
+	
+	@Test
+	void nested_brackets_with_multiple_operations_next_to_bracket() {
+		double result = ((1/1)+3)*3;
+		
+		assertEquals(result, calculator.evaluate("((1/1)+3)*3"));
+	}
+	
+	@Test
+	void multiple_nested_brackets_with_multiple_operations_next_to_bracket() {
+		double result = ((1+3)+4*6+(7*8))*8;
+		
+		assertEquals(result, calculator.evaluate("((1+3)+4*6+(7*8))*8"));
+	}
+	
+	@Test
+	void multiple_nested_brackets_with_multiple_operations_next_to_bracket_with_operation_outside_bracket() {
+		double result = ((1+3)+4*6+(7*8)-(4-(8*3)))*8;
+		
+		assertEquals(result, calculator.evaluate("((1+3)+4*6+(7*8)-(4-(8*3)))*8"));
+	}
+	
+	// Tests for simple exponential operations
+	
+	@Test
+	void single_exponent_operation() {
+		double result = Math.pow(2, 2);
+		
+		assertEquals(result, calculator.evaluate("2^2"));
+	}
+	
+	@Test
+	void single_exponent_operation_zero_exponent() {
+		double result = Math.pow(2, 0);
+		
+		assertEquals(result, calculator.evaluate("2^0"));
+	}
+	
+	@Test
+	void single_exponent_operation_negative_exponent() {
+		double result = Math.pow(2, -1);
+		
+		assertEquals(result, calculator.evaluate("2^-1"));
+	}
+	
+	@Test
+	void single_exponent_operation_one_exponent() {
+		double result = Math.pow(2, 1);
+		
+		assertEquals(result, calculator.evaluate("2^1"));
+	}
+	
+	@Test
+	void single_exponent_operation_decimal_base() {
+		double result = Math.pow(2.8, 2);
+		
+		assertEquals(result, calculator.evaluate("2.8^2"));
+	}
+	
+	@Test
+	void single_exponent_operation_with_multiple_operations() {
+		double result = 3*Math.pow(2, 2)/5;
+		
+		assertEquals(result, calculator.evaluate("3*2^2/5"));
+	}
+	
+	@Test
+	void single_exponent_operation_with_multiple_operations_and_negative() {
+		double result = -3*-1*Math.pow(2, 2)/5+5*8;
+		
+		assertEquals(result, calculator.evaluate("-3*-2^2/5+5*8"));
+	}
+	
+	@Test
+	void multiple_exponent_operation_with_multiple_operations() {
+		double result = -3*-1*Math.pow(2, 2)/5+5*8+Math.pow(3, 2);
+		
+		assertEquals(result, calculator.evaluate("-3*-2^2/5+5*8+3^2"));
+	}
+	
+	// Tests for exponential operations with parenthesis
+	
+	@Test
+	void exponent_operation_with_brackets() {
+		double result = Math.pow(2,(2*2));
+		
+		assertEquals(result, calculator.evaluate("2^(2*2)"));
+	}
+	
+	@Test
+	void nested_exponent_operation_with_brackets() {
+		
+		assertEquals(196631, calculator.evaluate("68+2^(2*2)^2*3-45"));
+	}
+	
+	
+	// Tests for more complex calculations
+	
+	@Test
+	void nested_brackets_multiple_operations() {
+		double result = 4+(3*(2+1)) - Math.pow(3,2);
+		
+		assertEquals(result, calculator.evaluate("4 + (3 * (2 + 1)) - (5 - 2)^2"));
+	}
+	
+	@Test
+	void multiple_brackets_with_multiple_exponent_operations() {
+		double result = (8+1) * (3+16);
+		
+		assertEquals(result, calculator.evaluate("(2^3 + 1) * (3 + 4^2)"));
+	}
+	
+	@Test
+	void nested_brackets_with_multiple_operations_and_outside_exponent() {
+		double result =2*Math.pow((3+(4-1)*3),2);
+		
+		assertEquals(result, calculator.evaluate("2 * (3 + (4 - 1) * (6 / 2))^2"));
+	}
+	
+	@Test
+	void final_test_all_operations() {
+		
+		assertEquals(-90.5041667, calculator.evaluate("-3*((2)^2/5+5*8)-(4+3*(2.5^2-(1/3)^2)/4)+(7-2.5)*(6/2)^2"), 0.0001);
+
+	}
+	
+}
